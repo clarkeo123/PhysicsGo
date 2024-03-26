e208b85ae606c4417a59d6f02dc05c6a0b827217
clarkeo123 <clarkeo123@outlook.com>
2024-03-26 16:05:48 +0000

    Draws solid circles where my mouse is

--- DopplerEffect.py
+++ DopplerEffect.py
@@ -4,14 +4,31 @@ pygame.init()
 clock = pygame.time.Clock()
 resolution = pygame.display.get_desktop_sizes()[0]
 
+class wave:
+    def __init__(self):
+        self.centre = pygame.mouse.get_pos()
+        self.radius = 50
+
+    def update(self):
+        pygame.draw.circle(screen, (0,0,0), self.centre, self.radius)
+
 screen = pygame.display.set_mode(resolution)
 pygame.display.set_caption('Doppler test and that')
+
+wavelist = []
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((255,255,255))
+
+    wavelist.append(wave())
+    
+    for obj in wavelist:
+        obj.update()
 
     pygame.display.update()
     clock.tick(60)
