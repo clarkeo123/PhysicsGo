14a073ec729661e8381451fff6a052d45d10a0af
clarkeo123 <clarkeo123@outlook.com>
2024-08-01 15:39:08 +0100

    Added slider to control block speed

--- Kinematics.py
+++ Kinematics.py
@@ -19,11 +19,51 @@ class object:
         self.rect.left += self.speed
         pygame.draw.rect(screen,(0,0,0),self.rect)
 
+class slider:
+    def __init__(self, startx, endx, height, startvalue, endvalue):
+        #sets all of the initial attributes of the slider
+        self.startx = startx
+        self.endx = endx
+        self.height = height
+        self.startvalue = startvalue
+        self.endvalue = endvalue
+        self.sliderx = resolution[0]//2
+        self.hovered = False
+        self.clicked = False
+        self.slidersize = (resolution[1]//58)
+        self.linesize = resolution[1]//120
+
+    def update(self):
+        #draws the line and circle which make up the slider
+        pygame.draw.line(screen, (0,0,0), [self.startx,self.height], [self.endx,self.height], self.linesize)
+        slider = pygame.draw.circle(screen, (0,0,0), [self.sliderx,self.height], self.slidersize)
+        #checks if the slider is being hovered over by the mouse
+        if slider.collidepoint(pygame.mouse.get_pos()):
+            self.hovered = True
+        else:
+            self.hovered = False
+        #checks if the slider is being cliicked on by the mouse
+        if self.hovered and pygame.mouse.get_pressed(num_buttons=3)[0]:
+            self.clicked = True
+        elif pygame.mouse.get_pressed(num_buttons=3)[0] == False:
+            self.clicked = False
+        #makes the slider move with the mouse if it is being clicked on
+        if self.clicked:
+            self.sliderx = pygame.mouse.get_pos()[0]
+        #stops the slider from going off the line
+        if self.sliderx < self.startx:
+            self.sliderx = self.startx
+        elif self.sliderx > self.endx:
+            self.sliderx = self.endx
+        #calculates the value which the slider is on based on its position, length and chosen start and end values
+        return round((((self.sliderx-self.startx)/(self.endx-self.startx))*(self.endvalue-self.startvalue))+self.startvalue)
+
 #fullscreens the pygame window and gives it a title
 screen = pygame.display.set_mode(resolution)
 pygame.display.set_caption('Kinematics Simulation')
 
 block = object()
+thrustslider = slider(resolution[0]//5,resolution[0]//(5/4),resolution[1]//(10/9),0,25)
 
 #main loop
 running = True
@@ -35,6 +75,7 @@ while running:
 
     screen.fill((255,255,255))
 
+    block.speed = thrustslider.update()
     block.update()
 
     pygame.display.update()
