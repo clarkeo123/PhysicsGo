133aeb59b14142e24379f320869d36c09fa22b57
clarkeo123 <clarkeo123@outlook.com>
2024-08-01 15:23:34 +0100

    Opens blank window

---
+++ Kinematics.py
@@ -0,0 +1,27 @@
+#modules
+import pygame
+import random
+import math
+
+#initialises the pygame window and creates the fps clock
+pygame.init()
+clock = pygame.time.Clock()
+resolution = pygame.display.get_desktop_sizes()[0] #gets the resolution of the display
+
+#fullscreens the pygame window and gives it a title
+screen = pygame.display.set_mode(resolution)
+pygame.display.set_caption('Kinematics Simulation')
+
+#main loop
+running = True
+while running:
+    #stops the loop if the pygame window is closed
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255,255,255))
+
+    pygame.display.update()
+    clock.tick(60)
+quit()
