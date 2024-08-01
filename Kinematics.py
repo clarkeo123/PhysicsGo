d8419e0dad49d6261f31ae01d060aa0fcc0a3a76
clarkeo123 <clarkeo123@outlook.com>
2024-08-01 15:30:30 +0100

    Made a box that moves across the screen

--- Kinematics.py
+++ Kinematics.py
@@ -8,9 +8,20 @@ pygame.init()
 clock = pygame.time.Clock()
 resolution = pygame.display.get_desktop_sizes()[0] #gets the resolution of the display
 
+class object:
+    def __init__(self):
+        self.rect = pygame.Rect(resolution[0]//(20/9),resolution[1]//(20/9),resolution[0]//10,resolution[1]//10)
+        self.speed = 10
+
+    def update(self):
+        self.rect.left += self.speed
+        pygame.draw.rect(screen,(0,0,0),self.rect)
+
 #fullscreens the pygame window and gives it a title
 screen = pygame.display.set_mode(resolution)
 pygame.display.set_caption('Kinematics Simulation')
+
+block = object()
 
 #main loop
 running = True
@@ -21,6 +32,8 @@ while running:
             running = False
 
     screen.fill((255,255,255))
+
+    block.update()
 
     pygame.display.update()
     clock.tick(60)
