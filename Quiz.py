c25ad049ba2af8f8b6e042e4f160a6d8704b9061
clarkeo123 <clarkeo123@outlook.com>
2024-08-07 18:33:06 +0100

    Quiz now pulls questions from json file

--- Quiz.py
+++ Quiz.py
@@ -1,10 +1,15 @@
 from guizero import *
+import json
+
+file = open("QuizAnswers.json")
+data = json.load(file)["Doppler Effect Quiz"]
+questioncounter = 0
 
 app = App(title="Physics Quiz")
 
-title = Text(app, text="Question 1", size=64)
+title = Text(app, text=f"Question {questioncounter+1}", size=64)
 filler1 = Text(app,text="",size=32)
-question = Text(app, text="Insert Question Here", size=32)
+question = Text(app, text=data[questioncounter]["question"], size=32)
 filler2 = Text(app,text="",size=32)
 answerbox = TextBox(app, width=32)
 filler3 = Text(app,text="",size=32)
