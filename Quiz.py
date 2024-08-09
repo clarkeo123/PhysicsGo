8a37a60122051713b02ac79772bb34b5368dcd8f
clarkeo123 <clarkeo123@outlook.com>
2024-08-09 16:24:43 +0100

    Displays a score page at the end of the questions

--- Quiz.py
+++ Quiz.py
@@ -4,8 +4,26 @@ import json
 file = open("QuizAnswers.json")
 data = json.load(file)["Doppler Effect Quiz"]
 questioncounter = 0
-
+score = 0
 app = App(title="Physics Quiz")
+
+def endoftest():
+    title.value = f"Score: {score}/{len(data)}"
+    question.visible = False
+    answerbox.visible = False
+    submitbutton.text= "Exit"
+    submitbutton._command = quit
+
+
+def checkanswer():
+    global questioncounter
+    if questioncounter < len(data)-1:
+        questioncounter += 1
+        title.value = f"Question {questioncounter+1}"
+        question.value = data[questioncounter]["question"]
+    else:
+        endoftest()
+        
 
 title = Text(app, text=f"Question {questioncounter+1}", size=64)
 filler1 = Text(app,text="",size=32)
@@ -13,6 +31,6 @@ question = Text(app, text=data[questioncounter]["question"], size=32)
 filler2 = Text(app,text="",size=32)
 answerbox = TextBox(app, width=32)
 filler3 = Text(app,text="",size=32)
-submitbutton = PushButton(app, text="Submit")
+submitbutton = PushButton(app, text="Submit", command=checkanswer)
 
 app.display()
