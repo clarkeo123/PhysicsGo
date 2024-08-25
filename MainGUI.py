65f01dacf6c54a1c1c8e3b5e04342e8253fa5007
clarkeo123 <clarkeo123@outlook.com>
2024-08-25 16:49:32 +0100

    Added a menu

--- MainGUI.py
+++ MainGUI.py
@@ -5,31 +5,59 @@ file = open("Logins.json")
 data = json.load(file)["Credentials"]
 
 app = App(title="PhysicsGo")
-app.set_full_screen()
+app.hide()
+
+def backtologin(currentwindow):
+    global useraccess
+    currentwindow.destroy()
+    useraccess = ""
+    loginscreen()
 
 def loginchecker(username, password):
+    global useraccess
     if username.value == data[0]["Username"] and password.value == data[0]["Password"]:
-        loginresponse.value = "Teacher logged in"
+        useraccess = "Teacher"
+        loginwindow.destroy()
+        menu()
     elif username.value == data[1]["Username"] and password.value == data[1]["Password"]:
-        loginresponse.value = "Student logged in"
+        useraccess = "Student"
+        loginwindow.destroy()
+        menu()
     else:
         loginresponse.value = "Incorrect username or password"
 
 def loginscreen():
-    global loginresponse
-    title = Text(app, text="Welcome to PhysicsGo!", size=72)
-    fillertextb = Text(app, text="", size=50)
-    usernametext = Text(app, text="Username")
-    usernamebox = TextBox(app, width=25)
-    fillertextc = Text(app, text="")
-    passwordtext = Text(app, text="Password")
-    passwordbox = TextBox(app, width=25, hide_text=True)
-    fillertextf = Text(app, text="")
-    loginresponse = Text(app, text="")
-    fillertextd = Text(app, text="")
-    enterbutton = PushButton(app, text="Log In", command=loginchecker, args=[usernamebox, passwordbox])
-    fillertexte = Text(app, text="", size=64)
-    exitbutton = PushButton(app, text="Exit", command=quit)
+    global loginresponse, loginwindow
+    loginwindow = Window(app, title="balls")
+    loginwindow.set_full_screen()
+    title = Text(loginwindow, text="Welcome to PhysicsGo!", size=72)
+    fillertextb = Text(loginwindow, text="", size=50)
+    usernametext = Text(loginwindow, text="Username")
+    usernamebox = TextBox(loginwindow, width=25)
+    fillertextc = Text(loginwindow, text="")
+    passwordtext = Text(loginwindow, text="Password")
+    passwordbox = TextBox(loginwindow, width=25, hide_text=True)
+    fillertextf = Text(loginwindow, text="")
+    loginresponse = Text(loginwindow, text="")
+    fillertextd = Text(loginwindow, text="")
+    enterbutton = PushButton(loginwindow, text="Log In", command=loginchecker, args=[usernamebox, passwordbox])
+    fillertexte = Text(loginwindow, text="", size=64)
+    exitbutton = PushButton(loginwindow, text="Exit", command=quit)
+
+def menu():
+    global menuwindow
+    menuwindow = Window(app, title="Second window")
+    menuwindow.set_full_screen()
+    title = Text(menuwindow, text="Please choose an option:", size=32)
+    fillertexta = Text(menuwindow, text="")
+    doppleroption = PushButton(menuwindow, text="Doppler Effect Simulation")
+    dopplerquizoption = PushButton(menuwindow, text="Doppler Effect Quiz")
+    pressureoption = PushButton(menuwindow, text="Pressure Simulation")
+    pressurequizoption = PushButton(menuwindow, text="Pressure Quiz")
+    kinematicsoption = PushButton(menuwindow, text="Kinematics Simulation")
+    kinematicsquizoption = PushButton(menuwindow, text="Kinematics Quiz")
+    fillertextb = Text(menuwindow, text="")
+    logout = PushButton(menuwindow, text="Log Out", command=backtologin, args=[menuwindow])
 
 loginscreen()
 
