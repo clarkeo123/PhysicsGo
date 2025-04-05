35fa058b6ed3693bc5a1980b75c9fbb4b44ba1bd
clarkeo123 <clarkeo123@outlook.com>
2025-04-05 16:15:33 +0100

    Comments and minor bug fixes

--- MainGUI.py
+++ MainGUI.py
@@ -1,30 +1,39 @@
 from guizero import *
 import json
 import subprocess
+import os
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+#gets the login details from the database
 file = open("Logins.json")
 data = json.load(file)["Credentials"]
 
+#creates the master app object
 app = App(title="PhysicsGo")
 app.hide()
 
 def backtologin(currentwindow):
+    #closes the current window and returns them to the login page
     global useraccess
     currentwindow.destroy()
     useraccess = ""
     loginscreen()
 
 def runprogram(currentwindow,program):
+    #closes the menu and opens a simulation
     currentwindow.destroy()
     subprocess.run(['python',program])
     menu()
 
 def runquiz(currentwindow, quiz):
+    #replaces the menu objects with a quiz
     import Quiz
     Quiz.main(quiz, app)
 
 def loginchecker(username, password):
     global useraccess
+    #compares username and password input by the user with the database's login details
     if username.value == data[0]["Username"] and password.value == data[0]["Password"]:
         useraccess = "Teacher"
         loginwindow.destroy()
@@ -37,6 +46,7 @@ def loginchecker(username, password):
         loginresponse.value = "Incorrect username or password"
 
 def loginscreen():
+    #creates all gui objects for the login screen
     global loginresponse, loginwindow
     loginwindow = Window(app, title="balls")
     loginwindow.set_full_screen()
@@ -50,16 +60,18 @@ def loginscreen():
     fillertextf = Text(loginwindow, text="")
     loginresponse = Text(loginwindow, text="")
     fillertextd = Text(loginwindow, text="")
-    enterbutton = PushButton(loginwindow, text="Log In", command=loginchecker, args=[usernamebox, passwordbox])
+    enterbutton = PushButton(loginwindow, text="Log In", command=loginchecker, args=[usernamebox, passwordbox]) #triggers the login checker
     fillertexte = Text(loginwindow, text="", size=64)
     exitbutton = PushButton(loginwindow, text="Exit", command=quit)
 
 def menu():
+    #creates all gui objects for the main menu
     global menuwindow
     menuwindow = Window(app, title="Second window")
     menuwindow.set_full_screen()
     title = Text(menuwindow, text="Please choose an option:", size=32)
     fillertexta = Text(menuwindow, text="")
+    #each button calls a simulation or quiz
     doppleroption = PushButton(menuwindow, text="Doppler Effect Simulation", command=runprogram, args=[menuwindow,"DopplerEffect.py"])
     dopplerquizoption = PushButton(menuwindow, text="Doppler Effect Quiz", command=runquiz, args=[menuwindow, "Doppler Effect Quiz"])
     pressureoption = PushButton(menuwindow, text="Pressure Simulation", command=runprogram, args=[menuwindow,"ParticlePressure.py"])
